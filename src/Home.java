d21727d29e320239a0f4d2572bd038b40601c4cf
Valery <valery@MacBook-Air-Valery.local>
2016-11-23 19:50:56 +0200

    Initial commit

---
+++ src/Home.java
@@ -0,0 +1,6 @@
+         public class Home {
+             public static void main(String[] args) {
+                 System.out.println("Я думаю");
+                 System.out.print("быть программистом-это круто");
+             }
+         }
